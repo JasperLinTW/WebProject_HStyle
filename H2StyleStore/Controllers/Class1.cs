b30f600bf8b66e34f47f695b4af5b7591137cb08
ching <vunvun0213@gmail.com>
2022-12-28 11:39:03 +0800

    新增class1測試-1

---
+++ H2StyleStore/Controllers/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace H2StyleStore.Controllers
+{
+    public class Class1
+    {
+
+        //////////
+
+    }
+}
