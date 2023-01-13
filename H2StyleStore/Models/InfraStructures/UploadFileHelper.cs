5b1ec13b7910bd402f26318e33aadd890e061d31
Jasper <guirar1859@gmail.com>
2023-01-13 22:37:42 +0800

    uploadFileHelper

--- H2StyleStore/Models/InfraStructures/UploadFileHelper.cs
+++ H2StyleStore/Models/InfraStructures/UploadFileHelper.cs
@@ -7,30 +7,29 @@ namespace H2StyleStore.Models.Infrastructures
 {
 	public class UploadFileHelper
 	{
-		/// <summary>
-		/// 將上傳檔案存檔
-		/// </summary>
-		/// <param name="path">要存放的實體路徑</param>
-		/// <param name="file"></param>
-		/// <exception cref="UploadFileNullException">沒有上傳檔案,或檔案大小為 0 byte 時, 丟出本例外</exception>
-		/// <returns></returns>
+		///<summary>
+		///將檔案上傳存檔
+		///</summary>
+		///<param name="path">要存放的實體路徑</param>
+		///<param name="file"></param>>
+		///<exception cref="UploadFileNullException">沒有上傳檔案,或檔案大小為 0 byte 時, 丟出本例外</exception>
+		///<returns></returns>>
 		public string SaveAs(string path, HttpPostedFileBase file)
 		{
+
 			if (file == null || string.IsNullOrEmpty(file.FileName) || file.ContentLength == 0)
 			{
 				throw new UploadFileNullException();
 			}
 
-			string ext = System.IO.Path.GetExtension(file.FileName); // 副檔名, 例如 .jpg
-			string newFileName = Guid.NewGuid().ToString("N") + ext;
-			string fullPath = System.IO.Path.Combine(path, newFileName);
+			string ext= System.IO.Path.GetExtension(file.FileName);
+			string newFileName= Guid.NewGuid().ToString("N") + ext;
+			string fullPath= System.IO.Path.Combine(path, newFileName);
 
 			file.SaveAs(fullPath);
-
-			return $"../../Images/ProductImages/{newFileName}";
+			return newFileName;
 		}
 	}
-
 	public class UploadFileNullException : ApplicationException
 	{
 	}
