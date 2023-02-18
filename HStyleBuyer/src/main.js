1d4e999066afaa491c2d9356327ad60996d60664
JasperLinTW <115922849+JasperLinTW@users.noreply.github.com>
2023-02-18 10:38:03 +0800

    Delete HStyleBuyer directory
    del

--- HStyleBuyer/src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-axios.get("https://localhost:7243/api/Products").then(response => {
-	alert(response);
-})
-createApp(App).mount('#app')
