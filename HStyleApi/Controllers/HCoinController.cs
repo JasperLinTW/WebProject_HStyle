d872c76b2663a90817e86e385cabf3362e6de5e2
peiyu <soyu84036@gmail.com>
2023-02-23 11:54:00 +0800

    花費活動

--- HStyleApi/Controllers/HCoinController.cs
+++ HStyleApi/Controllers/HCoinController.cs
@@ -28,25 +28,46 @@ namespace HStyleApi.Controllers
 		}
 
 		// GET api/<HCoinController>/5
+		// 得到打卡資料
 		[HttpGet("CheckIn/{memberId}")]
 		public async Task<HCheckInDTO> GetHCheckIn(int memberId)
-        {
-			// 活動Id和項目
-            int activityId_checkIn = 3;
-            return await _service.GetHCheckIn(memberId, activityId_checkIn);
-		}
-
-		// POST api/<HCoinController>
-		[HttpPost]
-		public void Post([FromBody] string value)
 		{
+			// 活動Id和項目
+			int activityId_checkIn = 3;
+			return await _service.GetHCheckIn(memberId, activityId_checkIn);
 		}
 
 		// PUT api/<HCoinController>/5
+		// 將打卡紀錄傳回資料庫
 		[HttpPut("CheckIn/{id}")]
-		public void PutCheckIn(int id, [FromBody] string value)
+		public async Task<ActionResult> PutCheckIn(int id)
 		{
-            string response = _service.PutCheckInById(id);			
+			string response = await _service.PutCheckInById(id);
+			return Ok(response);
+		}
+
+		// 取得花費活動的規則
+		[HttpGet("CostHCoin")]
+		public async Task<IEnumerable<HActivityDTO>> GetCostRule()
+		{
+			// 滿{多少}金額；可花費的上限，滿額的{多少}%
+			return await _service.GetCostHCoinRule();
+		}
+
+		// 取得會員的總HCoin金額
+		[HttpGet("TotalHCoin/{memberId}")]
+		public async Task<int> GetTotalHCoin(int memberId)
+		{
+			return await _service.GetTotalHCoinByMemberId(memberId);
+		}
+
+		// 將會員花費的HCoin記錄到
+		// POST api/<HCoinController>
+		[HttpPost("CostHCoin/{memberId}")]
+		public void PostCostHCoin(int memberId, [FromBody] int value)
+		{
+			// 將會員花費的HCoin記錄到會員的活動中
+			_service.PostCostHCoinByMemberId(memberId, value);
 		}
 
 		// DELETE api/<HCoinController>/5
