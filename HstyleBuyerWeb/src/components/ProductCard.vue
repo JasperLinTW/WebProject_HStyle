abb50bb38c88ed363a241709e97d46a33cc195af
YungYu Liao <h11830123@gmail.com>
2023-03-03 11:17:05 +0800

    filter

---
+++ HstyleBuyerWeb/src/components/ProductCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <!-- 在這裡添加您的HTML代碼 -->
+    </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'MyComponent',
+    data() {
+        return {
+            // 在這裡添加您的數據
+        };
+    },
+    methods: {
+        // 在這裡添加您的方法
+    },
+    mounted() {
+        // 在這裡添加組件初始化邏輯
+    }
+});
+</script>
+
+<style>
+/* 在這裡添加您的樣式 */
+</style>
--- HstyleBuyerWeb/src/views/Product.vue
+++ HstyleBuyerWeb/src/views/Product.vue
@@ -1,12 +1,61 @@
 <!-- Product.vue -->
 <template>
-    <div>
-      <h1>Welcome to the Product page!</h1>
-      <p>This is the product page of our app.</p>
+  <div class="m-5">
+    <div class="d-flex justify-content-start">
+      <div class="filter">
+        <div class="dropdown-toggle" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+          尺寸
+        </div>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <li><a class="dropdown-item" href="#">Menu item</a></li>
+          <li><a class="dropdown-item" href="#">Menu item</a></li>
+          <li><a class="dropdown-item" href="#">Menu item</a></li>
+        </ul>
+      </div>
+      <div class="filter">
+        <div class="dropdown-toggle" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+          類別
+        </div>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <li><a class="dropdown-item" href="#">Menu item</a></li>
+          <li><a class="dropdown-item" href="#">Menu item</a></li>
+          <li><a class="dropdown-item" href="#">Menu item</a></li>
+        </ul>
+      </div>
+      <div class="filter">
+        <div class="dropdown-toggle" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+          顏色
+        </div>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <li><a class="dropdown-item" href="#">Menu item</a></li>
+          <li><a class="dropdown-item" href="#">Menu item</a></li>
+          <li><a class="dropdown-item" href="#">Menu item</a></li>
+        </ul>
+      </div>
     </div>
+  </div>
 </template>
   
-  <script>
-  
-  </script>
-  <style></style>
+<script>
+export default {
+  components: {
+  },
+  data() {
+    return {
+      // 在這裡添加您的數據
+    };
+  },
+  methods: {
+    // 在這裡添加您的方法
+  },
+  mounted() {
+    // 在這裡添加組件初始化邏輯
+  }
+};
+</script>
+
+<style scoped>
+.filter {
+  margin-right: 5%;
+}
+</style>
