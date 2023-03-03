2993a9b99d58fa7bd71738108bd1d9ba54e97fc9
YungYu Liao <h11830123@gmail.com>
2023-03-03 12:54:49 +0800

    TODO productcard

--- HstyleBuyerWeb/src/components/ProductCard.vue
+++ HstyleBuyerWeb/src/components/ProductCard.vue
@@ -1,13 +1,20 @@
 <template>
-    <div>
-        <!-- 在這裡添加您的HTML代碼 -->
+    <div class="col-lg-4">
+        <div class="card mb-4 shadow-sm">
+            <img class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">13256</h5>
+                <div class="d-flex justify-content-between align-items-center">
+                    <span class="price">56486</span>
+                    <button type="button" class="btn btn-sm btn-outline-primary">加入購物車</button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
-
-export default defineComponent({
+export default {
     name: 'MyComponent',
     data() {
         return {
@@ -20,7 +27,7 @@ export default defineComponent({
     mounted() {
         // 在這裡添加組件初始化邏輯
     }
-});
+};
 </script>
 
 <style>
--- HstyleBuyerWeb/src/views/Product.vue
+++ HstyleBuyerWeb/src/views/Product.vue
@@ -1,44 +1,73 @@
 <!-- Product.vue -->
 <template>
-  <div class="m-5">
-    <div class="d-flex justify-content-start">
-      <div class="filter">
-        <div class="dropdown-toggle" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-          尺寸
+  <div class="m-5 container">
+    <div class="row">
+      <div class="col-lg-5 border-bottom">
+        <div class="d-flex justify-content-start">
+          <div class="filter">
+            <div class="dropdown-toggle" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+              排序
+            </div>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <li><a class="dropdown-item" href="#">Menu item</a></li>
+              <li><a class="dropdown-item" href="#">Menu item</a></li>
+              <li><a class="dropdown-item" href="#">Menu item</a></li>
+            </ul>
+          </div>
+          <div class="filter">
+            <div class="dropdown-toggle" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+              尺寸
+            </div>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <li><a class="dropdown-item" href="#">Menu item</a></li>
+              <li><a class="dropdown-item" href="#">Menu item</a></li>
+              <li><a class="dropdown-item" href="#">Menu item</a></li>
+            </ul>
+          </div>
+          <div class="filter">
+            <div class="dropdown-toggle" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+              類別
+            </div>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <li><a class="dropdown-item" href="#">Menu item</a></li>
+              <li><a class="dropdown-item" href="#">Menu item</a></li>
+              <li><a class="dropdown-item" href="#">Menu item</a></li>
+            </ul>
+          </div>
+          <div class="filter">
+            <div class="dropdown-toggle" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+              顏色
+            </div>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <li><a class="dropdown-item" href="#">Menu item</a></li>
+              <li><a class="dropdown-item" href="#">Menu item</a></li>
+              <li><a class="dropdown-item" href="#">Menu item</a></li>
+            </ul>
+          </div>
         </div>
-        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-          <li><a class="dropdown-item" href="#">Menu item</a></li>
-          <li><a class="dropdown-item" href="#">Menu item</a></li>
-          <li><a class="dropdown-item" href="#">Menu item</a></li>
-        </ul>
-      </div>
-      <div class="filter">
-        <div class="dropdown-toggle" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-          類別
-        </div>
-        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-          <li><a class="dropdown-item" href="#">Menu item</a></li>
-          <li><a class="dropdown-item" href="#">Menu item</a></li>
-          <li><a class="dropdown-item" href="#">Menu item</a></li>
-        </ul>
-      </div>
-      <div class="filter">
-        <div class="dropdown-toggle" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-          顏色
-        </div>
-        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-          <li><a class="dropdown-item" href="#">Menu item</a></li>
-          <li><a class="dropdown-item" href="#">Menu item</a></li>
-          <li><a class="dropdown-item" href="#">Menu item</a></li>
-        </ul>
       </div>
     </div>
   </div>
+
+  <div class="container">
+    <div class="row align-items-start">
+      <ProductCard></ProductCard>
+      <ProductCard></ProductCard>
+      <ProductCard></ProductCard>
+    </div>
+  </div>
+
+  <Back2Top></Back2Top>
 </template>
   
 <script>
+import Back2Top from '../components/Back2Top.vue'
+import ProductCard from '../components/ProductCard.vue'
+
 export default {
   components: {
+    Back2Top,
+    ProductCard
   },
   data() {
     return {
@@ -53,9 +82,10 @@ export default {
   }
 };
 </script>
-
 <style scoped>
 .filter {
-  margin-right: 5%;
+  margin-right: 15%;
+  padding-bottom: 0.5%;
+  font-size: 12pt;
 }
 </style>
