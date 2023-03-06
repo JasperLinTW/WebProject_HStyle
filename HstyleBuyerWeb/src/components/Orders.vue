74034900a80edc487645905f396a6df3cf6ab5fd
YungYuLiao <h11830123@gmail.com>
2023-03-06 21:45:18 +0800

    訂單頁 付款完成頁修改

--- HstyleBuyerWeb/src/components/Orders.vue
+++ HstyleBuyerWeb/src/components/Orders.vue
@@ -1,72 +1,74 @@
 <template>
     <div class="container my-5">
-        <div class="order-header border-bottom border-dark">
-            <div class="row mb-2 ps-4">
+        <div class="order-header border pt-2">
+            <div class="row mb-2 px-5">
                 <div class="col-2 text-center">訂單編號</div>
                 <div class="col-2 text-center">金額</div>
                 <div class="col-2 text-center">日期</div>
                 <div class="col-2 text-center">付款方式</div>
-                <div class="col-1 text-center">狀態</div>
+                <div class="col-1 text-center ms-2">狀態</div>
                 <div class="col-2 text-center">幫助</div>
             </div>
         </div>
-        <div class="accordion" id="accordionExample"> 
-       <div  v-for="(order, index) in orders" :key="order.orderId" class="accordion-item">
-    <div class="accordion-header" :id="'heading' + index">
-      <button class="accordion-button btn-order d-flex " type="button" data-bs-toggle="collapse" 
-        :data-bs-target="'#collapse' + index" 
-        :aria-expanded="index===0" 
-        :aria-controls="'collapse' + index">
-        <div class="col-2 text-center">{{ order.orderId }}</div>
-                <div class="col-2 text-center">NT$ {{ order.total }}</div>
-                <div class="col-2 text-center">{{ order.createdTime.slice(0, 10) }}</div>
-                <div class="col-2 text-center">{{ order.payment }}</div>
-                <div class="col-1 text-center">
-                    <div v-if="order.statusId === 1">
-                        <a :href="`https://www.sandbox.paypal.com/checkoutnow?token=${order.payInfo}`" class="alink">待付款</a>
+        <div class="accordion" id="accordionExample">
+            <div v-for="(order, index) in orders" :key="order.orderId" class="accordion-item">
+                <div class="accordion-header" :id="'heading' + index">
+                    <button class="accordion-button btn-order" type="button" data-bs-toggle="collapse"
+                        :data-bs-target="'#collapse' + index" :aria-expanded="index === 0"
+                        :aria-controls="'collapse' + index">
+                        <div class="col-2 text-center">{{ order.orderId }}</div>
+                        <div class="col-2 text-center">NT$ {{ order.total }}</div>
+                        <div class="col-2 text-center">{{ order.createdTime.slice(0, 10) }}</div>
+                        <div class="col-2 text-center">{{ order.payment }}</div>
+                        <div class="col-1 text-center ps-1">
+                            <div v-if="order.statusId === 1">
+                                <a @click.prevent="goToPay(order.payInfo)" class="alink">待付款<i
+                                        class="fa-solid fa-arrow-up-right-from-square fz-sm ps-2"></i></a>
+                            </div>
+                            <div v-else>
+                                {{ order.status }}
+                            </div>
+                        </div>
+                        <div class="col-2  text-center">
+                            <a>聯絡客服</a>
+                        </div>
+                    </button>
+                </div>
+                <div :id="'collapse' + index" class="accordion-collapse collapse" :class="{ show }"
+                    :aria-labelledby="'heading' + index" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <div class="row bg-dark text-white font-weight-bold  py-2">
+                            <div class="col-1"></div>
+                            <div class="col-2 text-center">品名</div>
+                            <div class="col-2 text-center">規格</div>
+                            <div class="col-2 text-center">單價</div>
+                            <div class="col-2 text-center">數量</div>
+                            <div class="col-2 text-center">小計</div>
+                            <div class="col-1"></div>
+                        </div>
+                        <div v-for="(product, index) in order.orderDetails" :key="index"
+                            class="row accordion-body border-start border-end py-3 px-0">
+                            <div class="col-1"></div>
+                            <div class="col-2 text-center">{{ product.productName }}</div>
+                            <div class="col-2 text-center">{{ product.color }}</div>
+                            <div class="col-2 text-center">{{ product.unitPrice }}</div>
+                            <div class="col-2 text-center">{{ product.quantity }}</div>
+                            <div class="col-2 text-center">{{ product.subTotal }}</div>
+                            <div class="col-1">我要評論</div>
+                        </div>
+                        <div class="row order-item-footer py-2 border ">
+                            <div class="col-1"></div>
+                            <div class="col-2  text-center">共 {{ order.orderDetails.length }} 件</div>
+                            <div class="col-6"></div>
+                            <div class="col-2 text-center">
+                                H幣折抵：{{ order.discount }}
+                            </div>
+                        </div>
                     </div>
-                    <div v-else>
-                        {{ order.status }}
-                    </div> 
                 </div>
-                <div class="col-2  text-center">
-                    <a>聯絡客服</a>
-                </div>
-      </button>
-    </div>
-    <div :id="'collapse' + index" class="accordion-collapse collapse" :class="{show}" :aria-labelledby="'heading' + index" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-       <div class="row bg-dark text-white font-weight-bold  py-2">
-                    <div class="col-1"></div>
-                    <div class="col-2">品名</div>
-                    <div class="col-2">規格</div>
-                    <div class="col-2">單價</div>
-                    <div class="col-2">數量</div>
-                    <div class="col-2">小計</div>
-                    <div class="col-1"></div>
-                </div>
-                <div v-for="(product, index) in order.orderDetails" :key="index" class="row accordion-body border-start border-end py-3">
-                    <div class="col-1"></div>
-                    <div class="col-2">{{ product.productName }}</div>
-                    <div class="col-2">{{ product.color }}</div>
-                    <div class="col-2">{{ product.unitPrice }}</div>
-                    <div class="col-2">{{ product.quantity }}</div>
-                    <div class="col-2">{{ product.subTotal }}</div>
-                    <div class="col-1">我要評論</div>
-                </div>
-                <div class="row order-item-footer py-1  border-start border-end border-bottom">
-                    <div class="col-1"></div>
-                    <div class="col-1">共 {{ order.orderDetails.length }} 件</div>
-                    <div class="col-6"></div>
-                    <div class="col-3">
-                        <div class=" text-right">H幣折抵：{{ order.discount }}</div>
-                    </div>
-                </div>
-      </div>
-    </div>
-  </div>
+            </div>
         </div>
-</div>
+    </div>
 </template>
   
   
@@ -78,20 +80,16 @@ const orders = ref([]);
 
 const getOrdersInfo = async () => {
     await axios.get("https://localhost:7243/api/Order")
-        .then(response => { 
+        .then(response => {
             orders.value = response.data;
             console.log(orders.value)
         })
         .catch(error => { console.log(error); });
 }
-const selectProduct = ref("");
-function showDetails(index) {
-    // orders.value[index].showDetails = !orders.value[index].showDetails;
-    selectProduct.value = index;
-}
-function hideDetails(index) {
-    // orders.value[index].showDetails = !orders.value[index].showDetails;
-    selectProduct.value = null;
+
+function goToPay(token) {
+    // 使用 window.location.href 跳轉到 PayPal 付款頁面
+    window.location.href = `https://www.sandbox.paypal.com/checkoutnow?token=${token}`;
 }
 
 function timeFormat(date) {
@@ -117,18 +115,15 @@ onMounted(() => {
 </script>
 
 <style scoped>
-.p-0 {
-    padding: 0 !important;
+.fz-sm {
+    font-size: 5pt;
+    color: gray;
 }
 
-.alink{
+.alink {
     text-decoration: none;
-    color: #46a3ff;
-    font-weight: bolder;
-}
-
-.alink:hover {
-    text-decoration-line: underline;
+    padding-left: 15pt;
+    color: #46A3FF;
 }
 
 .accordion-item {
@@ -142,13 +137,17 @@ onMounted(() => {
     --bs-accordion-border-radius: 0%;
 }
 
-.btn-order{
-    background: none;
-    color: #000;
-    box-shadow:none;
+.accordion-button {
+    --bs-accordion-btn-icon-width: 12pt;
 }
 
-.btn-order:focus{
+.btn-order {
+    background: none;
+    color: #000;
+    box-shadow: none;
+}
+
+.btn-order:focus {
     outline-color: none;
     box-shadow: none;
 }
--- HstyleBuyerWeb/src/views/OrderComplete.vue
+++ HstyleBuyerWeb/src/views/OrderComplete.vue
@@ -5,61 +5,60 @@
         <div class="card mt-6">
           <div class="card-header text-center fw-bold">付款成功</div>
           <div class="card-body text-center mb-4">
-            感謝您的付款
+            已收到訂單，感謝您的購買
           </div>
           <div class="text-center mt-5">
-            <router-link class="returnbtn" to="/product">點此繼續購物</router-link>  
+            <router-link class="returnbtn" to="/product">點此繼續購物</router-link>
+          </div>
         </div>
       </div>
     </div>
   </div>
-  </div>
 </template>
 
 <script>
-  
+
 </script>
 
 <style scoped>
-.card{
+.card {
   border: none;
 }
 
-.card-header{
-  border-radius:0%;
-  background-color: none;
+.card-header {
+  border-radius: 0%;
+  background-color: transparent;
   font-size: 15pt;
 }
 
-.a{
+.a {
   text-decoration: none;
   color: #000000;
 }
 
 .returnbtn:not(.nav-btns button) {
-    background-color: #fff;
-    color: rgb(12, 13, 12);
-    padding: 10px 28px;
-    border-radius: 25px;
-    border: 1px solid rgb(12, 13, 12);
-    transition: all 0.3s ease;
-    text-decoration: none;
+  background-color: #fff;
+  color: rgb(12, 13, 12);
+  padding: 10px 28px;
+  border-radius: 25px;
+  border: 1px solid rgb(12, 13, 12);
+  transition: all 0.3s ease;
+  text-decoration: none;
 
 }
 
 .returnbtn:not(.nav-btns button):hover {
-    background-color: #000000;
-    color: #fff;
-    ;
-    border-color: #000000;
-    text-decoration: none;
+  background-color: #000000;
+  color: #fff;
+  ;
+  border-color: #000000;
+  text-decoration: none;
 }
 
 
 
-.mt-6{
+.mt-6 {
 
   margin-top: 20%;
 }
-
 </style>
