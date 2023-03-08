d051f041fe05eeae128c0d014ce02423a89356e8
ching <vunvun0213@gmail.com>
2023-03-08 12:06:37 +0800

    -1

--- HstyleBuyerWeb/src/components/MemberProfile.vue
+++ HstyleBuyerWeb/src/components/MemberProfile.vue
@@ -2,9 +2,34 @@
     <div>
         <p>會員資料</p>
     </div>
+
+    <!-- <div v-for="m in member" class="row border-bottom py-3">
+                    <div class="col-1"></div>
+                    <div class="col-2">{{  }}</div>
+                    <div class="col-2">{{ product.color }}</div>
+                    <div class="col-2">{{ product.unitPrice }}</div>
+                    <div class="col-2">{{ product.quantity }}</div>
+                    <div class="col-2">{{ product.subTotal }}</div>
+    </div> -->
+
+
+
 </template>
     
-<script>
+<script setup>
+// import { onMounted, ref } from 'vue';
+// import axios from "axios";
 
+// const Member = ref([]);
+
+// const getOrdersInfo = async () => {
+//     await axios.get("https://localhost:7243/api/Member")
+//         .then(response => { Member.value = response.data; })
+//         .catch(error => { console.log(error); });
+// }
+
+// onMounted(() => {
+//     getOrdersInfo();
+// });
 </script>
 <style></style>
