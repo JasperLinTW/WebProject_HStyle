131f97acda0285d0bc1446766a3d8ad850c4b9b7
peiyu <soyu84036@gmail.com>
2023-03-10 16:06:53 +0800

    常見問題-搜尋

--- HstyleBuyerWeb/src/components/QResponses.vue
+++ HstyleBuyerWeb/src/components/QResponses.vue
@@ -1,27 +1,48 @@
 <template>
-   <table class="table">
-      <thead>
-         <tr>
-            <th scope="col">編號</th>
-            <th scope="col">問題</th>
-            <th scope="col">問題描述</th>
-            <th scope="col">詢問時間</th>
-            <th scope="col"></th>
-         </tr>
-      </thead>
-      <tbody>
-         <tr v-for="question in memberQ" :key="question.customerQuestionId">
-            <th scope="row">
-               <div v-if="question.solutionDescription == null"><i class="fa-regular fa-circle"></i></div>
-               <div v-else><i class="fa-solid fa-check"></i></div>
-            </th>
-            <td>{{ question.title }}</td>
-            <td>{{ question.problemDescription }}</td>
-            <td>{{ question.askTime }}</td>
-            <td>{{ question.solutionDescription }}</td>
-         </tr>
-      </tbody>
-   </table>
+   <div class="container my-5">
+      <div class="order-header border-top border-bottom pt-2">
+         <div class="row mb-2">
+            <div class="col-1 text-center"></div>
+            <div class="col-2 text-center">問題</div>
+            <div class="col-4 text-center">問題描述</div>
+            <div class="col-3 text-center">詢問時間</div>
+            <div class="col-1 text-center"></div>
+         </div>
+      </div>
+      <div class="accordion accordion-flush" id="accordionExample">
+         <div v-for="(question, index) in memberQ" :key="question.customerQuestionId" class="accordion-item">
+            <div class="accordion-header row" :id="'heading' + index">
+               <div class="col-1 text-center">
+                  <button
+                     class="accordion-button collapsed"
+                     type="button"
+                     data-bs-toggle="collapse"
+                     :data-bs-target="'#collapse' + index"                     
+                     aria-expanded="false"
+                     :aria-controls="'collapse' + index"
+                  ></button>
+               </div>
+               <div class="col-2 text-center pt-2">{{ question.title }}</div>
+               <div class="col-4 text-center pt-2">{{ question.problemDescription }}</div>
+               <div class="col-3 text-center pt-2">{{ question.askTime.slice(0, 10) }}</div>
+               <div class="col-1 text-center pt-2">
+                  <div v-if="question.solutionDescription == null"><i class="fa-regular fa-circle"></i></div>
+                  <div v-else><i class="fa-solid fa-check"></i></div>
+               </div>
+            </div>
+            <div
+               :id="'collapse' + index"
+               class="accordion-collapse collapse"
+               :aria-labelledby="'heading' + index"
+               data-bs-parent="#accordionExample"
+            >
+               <div class="accordion-body">
+                  {{ question.solutionDescription }}
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
 </template>
 
 <script setup>
@@ -52,4 +73,58 @@ onMounted(() => {
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.fz-sm {
+   font-size: 5pt;
+   color: gray;
+}
+
+.alink {
+   text-decoration: none;
+   padding-left: 15pt;
+   color: #46a3ff;
+   cursor: pointer;
+}
+
+.blink {
+   text-decoration: none;
+   color: #46a3ff;
+   cursor: pointer;
+}
+
+.accordion-item {
+   border-top: none;
+   border-radius: none;
+}
+
+.accordion {
+   background-color: transparent;
+   --bs-accordion-border-radius: 0%;
+}
+
+.accordion-button {
+   --bs-accordion-btn-icon-width: 12pt;
+}
+
+.btn-order {
+   background: none;
+   color: #000;
+   box-shadow: none;
+}
+
+.btn-order:focus {
+   outline-color: none;
+   box-shadow: none;
+}
+
+.button-comment:focus,
+.button-comment:active {
+   box-shadow: none;
+   outline: none;
+}
+
+.btn-link {
+   text-decoration-line: none;
+   color: #46a3ff;
+}
+</style>
