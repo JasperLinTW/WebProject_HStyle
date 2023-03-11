cee4db0acd9c94ed00ecf32ee97d0524f56f12c2
DeborahHuang <deborah990501@gmail.com>
2023-03-11 22:11:08 +0800

    card

--- HstyleBuyerWeb/src/components/EssayCard.vue
+++ HstyleBuyerWeb/src/components/EssayCard.vue
@@ -1,71 +1,79 @@
 <template>
   <!-- 内文解碼 -->
- 
-      <div class="col-md-3  mb-4">
-        <div class="card border-0 card1">
-          <div class="card-img w-100 h200px rounded overflow-hidden">
 
-            <img :src="data.imgs[0]" class="card-img-top" alt="Essays Image">
+  <div class="col-md-3 mb-4">
+    <router-link :to="'/EssaysBlog/' + data.essayId" class="text-dark text-decoration-none">
+      <div class="card border-0 card1">
+        <div class="card-img w-100 h200px rounded overflow-hidden">
+          <img :src="data.imgs[0]" class="card-img-top" alt="Essays Image" />
+        </div>
 
-          </div>
+        <div class="card-header d-flex bg-white border-bottom-0">
+          <span class="badge bg-secondary opacity-50 me-1" v-for="tag in data.tags">{{ tag }}</span>
+        </div>
+        <div class="card-body">
+          {{ data.etitle }}
+        </div>
+        <div class="card-footer bg-white border-top-0 d-flex">
+          <span class="me-auto">{{ data.uplodTime.slice(0, 10) }}</span>
 
-          <div class="card-header d-flex bg-white border-bottom-0">
-            <span class="badge bg-secondary opacity-50 me-1" v-for="tag in data.tags">{{ tag }}</span>
-        
-          </div>
-          <div class="card-body">
-       
-            <router-link :to="'/EssaysBlog/' + data.essayId" class="text-dark text-decoration-none stretched-link">{{
-              data.etitle }}</router-link>
-
-          </div>
-          <div class="card-footer bg-white border-top-0 d-flex">
-
-            <span class="me-auto">{{ data.uplodTime.slice(0, 10) }}</span>
-
-            <!-- <span><i class="fa-regular fa-bookmark"></i></span> -->
-            <div class="card-text text-end">
-              <span v-if="!data.isClicked" @click="data.isClicked = true"><i
-                  class="fa-regular fa-bookmark icon-hover fz-18"></i></span>
-              <span v-else @click="data.isClicked = false"><i class="fa-solid fa-bookmark SolidHeart fz-18"></i></span>
-            </div>
+          <!-- <span><i class="fa-regular fa-bookmark"></i></span> -->
+          <div @click.stop class="card-text text-end">
+            <span v-if="!data.isClicked" @click.prevent @click="postEssayLike(data)"><i class="fa-regular fa-bookmark icon-hover fz-18"></i></span>
+            <span v-else @click.prevent @click="postEssayLike(data)"><i class="fa-solid fa-bookmark SolidHeart fz-18"></i></span>
           </div>
         </div>
       </div>
+    </router-link>
+  </div>
 </template>
 <script setup>
 import { ref, onMounted } from "vue";
 import axios from "axios";
+// import { eventBus } from "../mybus";
 const props = defineProps({
   data: Object,
 });
-onMounted(() => {
-  // getEssayInfo();
-});
 
 //收藏
-let likes = ref([])
-
-const Essays = ref([])
-
-
+let likes = ref([]);
+const Essays = ref([]);
 
 const likesEssay = async () => {
-  await axios.get("https://localhost:7243/api/Essay/Elike")
-    .then(response => {
+  await axios
+    .get("https://localhost:7243/api/Essay/Elike")
+    .then((response) => {
       if (response.data.length > 0) {
         likes.value = response.data;
         console.log(likes.value);
-        Essays.value = likes.value.map(e => {
-          return e.essayId
+        Essays.value = likes.value.map((e) => {
+          return e.essayId;
         });
       }
-
     })
-    .catch(error => { console.log(error); });
-}
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
+const postEssayLike = async (data) => {
+  await axios
+    .post(`https://localhost:7243/api/Essay/Elike?essayId=${props.data.essayId}`, {}, { withCredentials: true })
+    .then((response) => {
+      data.isClicked = !data.isClicked;
+    })
+    .catch((error) => {
+      console.log(error);
+      if (error.response.status === 401) {
+        router.push("/login");
+      }
+    });
+};
 
+onMounted(async () => {
+  // getEssayInfo();
+  await likesEssay();
+});
 </script>
 <style scoped>
 .h200px {
