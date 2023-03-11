042e1dfc3353b492c4cf8ec1306ae1402d670fb5
YungYuLiao <h11830123@gmail.com>
2023-03-11 18:15:20 +0800

    篩選按鈕完成 功能待做

--- HstyleBuyerWeb/src/views/Product.vue
+++ HstyleBuyerWeb/src/views/Product.vue
@@ -1,6 +1,6 @@
 <!-- Product.vue -->
 <template>
-  <!-- <div class="m-5 container">
+  <div class="m-5 container">
     <div class="row">
       <div class="col-lg-5 border-bottom">
         <div class="d-flex justify-content-start">
@@ -8,25 +8,16 @@
             <div class="dropdown-toggle" id="sortDropdown" data-bs-toggle="dropdown" aria-expanded="false">
               排序
             </div>
-            <ul class="dropdown-menu" aria-labelledby="sortDropdown">
+            <ul class="dropdown-menu menu border-0 mt-1" aria-labelledby="sortDropdown">
               <li><a class="dropdown-item" href="#" v-for="(option, index) in sortOptions" :key="index"
                   @click="setSortOption(option)">{{ option }}</a></li>
-            </ul>
-          </div>
-          <div class="filter">
-            <div class="dropdown-toggle" id="sizeDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-              尺寸
-            </div>
-            <ul class="dropdown-menu" aria-labelledby="sizeDropdown">
-              <li><a class="dropdown-item" href="#" v-for="(option, index) in sizeOptions" :key="index"
-                  @click="setSizeOption(option)">{{ option }}</a></li>
             </ul>
           </div>
           <div class="filter">
             <div class="dropdown-toggle" id="categoryDropdown" data-bs-toggle="dropdown" aria-expanded="false">
               類別
             </div>
-            <ul class="dropdown-menu" aria-labelledby="categoryDropdown">
+            <ul class="dropdown-menu menu border-0 mt-1" aria-labelledby="categoryDropdown">
               <li><a class="dropdown-item" href="#" v-for="(option, index) in categoryOptions" :key="index"
                   @click="setCategoryOption(option)">{{ option }}</a></li>
             </ul>
@@ -35,15 +26,30 @@
             <div class="dropdown-toggle" id="colorDropdown" data-bs-toggle="dropdown" aria-expanded="false">
               顏色
             </div>
-            <ul class="dropdown-menu" aria-labelledby="colorDropdown">
-              <li><a class="dropdown-item" href="#" v-for="(option, index) in colorOptions" :key="index"
-                  @click="setColorOption(option)">{{ option }}</a></li>
+            <ul class="dropdown-menu menu border-0 mt-1 " aria-labelledby="colorDropdown">
+              <div class="row">
+                <li class="col-md-1">
+                  <a class="dropdown-item item " href="#"
+                    v-for="(option, index) in colorOptions.slice(0, Math.ceil(colorOptions.length / 2))" :key="index"
+                    @click="setColorOption(option)">
+                    {{ option }}
+                  </a>
+                </li>
+                <li class="col-md-1">
+                  <a class="dropdown-item item" href="#"
+                    v-for="(option, index) in colorOptions.slice(Math.ceil(colorOptions.length / 2),)" :key="index"
+                    @click="setColorOption(option)">
+                    {{ option }}
+                  </a>
+                </li>
+              </div>
+
             </ul>
           </div>
         </div>
       </div>
     </div>
-      </div> -->
+  </div>
 
   <div class="container">
     <div class="row">
@@ -65,6 +71,11 @@ import { eventBus } from "../mybus";
 const products = ref([]);
 const route = useRoute();
 //console.log(route.params.tag);
+
+//商品篩選排序
+const categoryOptions = ref([]);
+const colorOptions = ref([]);
+const sortOptions = ref(['新到舊', '舊到新', '價格高到低', '價格低到高']);
 //商品收藏
 const likeProductsId = ref([]);
 let likes = ref([]);
@@ -98,6 +109,11 @@ const loadProducts = async () => {
       response.data.map((p) => {
         p.isClicked = likeProductsId.value.includes(p.product_Id);
       });
+
+      //把篩選的先列出
+      categoryOptions.value = [...new Set(response.data.map((p) => p.pCategoryName))];
+      colorOptions.value = Array.from(new Set(response.data.flatMap((p) => p.specs.map((s) => s.color))));
+      console.log(colorOptions.value);
       if (route.params.tag == "new") {
         products.value = response.data.filter((p) => p.tags.includes("新品"));
       } else {
@@ -127,5 +143,23 @@ onMounted(() => {
   margin-right: 15%;
   padding-bottom: 0.5%;
   font-size: 12pt;
+
+}
+
+.menu {
+  min-width: 100%;
+  min-height: 21%;
+  border: none;
+  background-color: #ffffff;
+  padding-left: 2%;
+}
+
+.dropdown-menu a:hover {
+  background-color: transparent;
+  color: #46a3ff;
+}
+
+.dropdown-toggle {
+  cursor: pointer;
 }
 </style>
